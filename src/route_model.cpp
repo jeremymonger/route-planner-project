4cf04a9cc4a56d1692bca505b845a297426db03d
Jeremy Monger <jeremymong@gmail.com>
2019-06-16 14:22:07 -0400

    Bug fixing

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-  i = 0
+  int i = 0;
 
   for (Model::Node node : this-> Nodes()){
-    new_node = Node(int i, this, node);
+    auto new_node = Node( i, this, node);
     m_Nodes.push_back(new_node);
     i++;
-)
+
   }
 }
